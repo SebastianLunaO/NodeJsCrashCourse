4c8b5de622fcb172b8525d64fb48fb8a46e981b1
Sebastian Luna <newmsloh@gmail.com>
2024-12-16 17:04:12 -0600

    Making request by postman

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req,res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
+   
+    console.log(req.url);
+    console.log(req.method);
     res.writeHead(200, {'Content-Type': 'text/html'})
     res.end('<h1>hello</h1>');
 });
+
 
 server.listen(PORT, () => {console.log(`server running in port ${PORT}`)});
