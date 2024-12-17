47a9a156c32ee2513b2614bf10fa4e85a181fc7c
Sebastian Luna <newmsloh@gmail.com>
2024-12-16 21:32:53 -0600

    Using the POST method to add users to the 'Data Base'

--- Server2.js
+++ Server2.js
@@ -44,6 +44,22 @@ const notFoundHandler= (req,res) =>{
     res.end();
 }
 
+//Route Handler for POST /api/users
+const createUserHandler = (req,res) =>{
+    let body = '';
+    //listen for data
+    req.on('data',(chunk)=>{
+        body +=chunk.toString();
+    });
+    req.on('end',()=> {
+        const newUser = JSON.parse(body);
+        users.push(newUser);
+        res.statusCode=201;
+        res.write(JSON.stringify(newUser));
+        res.end()
+    });
+}
+
 const server = createServer((req,res) => {
     logger(req,res,()=>{
         jsonMiddleware(req,res,()=>{
@@ -52,6 +68,8 @@ const server = createServer((req,res) => {
 
             } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
                 getUserByID(req,res);
+            }else if (req.url === '/api/users' && req.method==='POST'){
+                createUserHandler(req,res)
             }else{
                 notFoundHandler(req,res);
             }
