fb02e61a19f1b35f00d4c77993dbe1fe9cd2462f
Sebastian Luna <newmsloh@gmail.com>
2024-12-16 21:20:57 -0600

    Adding Handlers and middleware to a better manage of the requests

--- Server2.js
+++ Server2.js
@@ -10,38 +10,56 @@ const users = [
 const logger = (req,res,next)=>{
     console.log(`${req.method} ${req.url}`);
     next();
+};
+//json middleware
+const jsonMiddleware = (req,res,next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}
+
+//route Handler for GET /api/users
+const getUsersHandler = (req,res) =>{
+    res.write(JSON.stringify(users));
+    res.end();
+}
+
+//route Handler for GET /users/:id
+const getUserByID = (req,res) =>{
+    const id = req.url.split('/')[3];
+    
+    const user=users.find((user)=> user.id === parseInt(id))
+
+    if (user){
+        res.write(JSON.stringify(user));
+    }else{
+        res.statusCode=404;
+        res.write(JSON.stringify({message: 'Route Not Found'}));
+    }
+    res.end();
+}
+//Not found handler
+const notFoundHandler= (req,res) =>{
+    res.statusCode=404;
+    res.write(JSON.stringify({message: 'Route Not Found'}));
+    res.end();
 }
 
 const server = createServer((req,res) => {
+    logger(req,res,()=>{
+        jsonMiddleware(req,res,()=>{
+            if (req.url === '/api/users' && req.method==='GET'){
+                getUsersHandler(req,res);
+
+            } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+                getUserByID(req,res);
+            }else{
+                notFoundHandler(req,res);
+            }
+        });
+    });
     
-    if (req.url === '/api/users' && req.method==='GET'){
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(users));
-        res.end();
-    }
-        else if(req.url.match(/\/api\/users\/([0-9]+)/)){
-        const id=req.url.split('/')[3];
-        const user=users.find((user)=> user.id === parseInt(id))
-        res.setHeader('Content-Type', 'application/json');
-        
-        if (user){
-            res.write(JSON.stringify(user));
-        }else{
-            res.statusCode=404;
-            res.write(JSON.stringify({message: 'Route Not Found'}));
-        }
-        res.end();
-
-
-        }
-    else{
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode=404;
-            res.write(JSON.stringify({message: 'Route Not Found'}));
-            res.end();
-    }
-})
+});
 
 server.listen(PORT,()=>{
     console.log(`Server running in port: ${PORT}`);
-})
+});
